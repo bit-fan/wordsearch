87a661c12967bf68aa8ff7e8fcca850c9a100d7c
EPAM\Lizhu_Wang <taur57@hotmail.com>
2022-09-22 18:30:10 +0800

    Updates

---
+++ precache-manifest.ae1af54607e22ea253c5722d204699e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a7a8716fbb046585c4",
+    "url": "/wordsearch/static/css/main.2ab4fcc5.chunk.css"
+  },
+  {
+    "revision": "77a7a8716fbb046585c4",
+    "url": "/wordsearch/static/js/main.77a7a871.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/wordsearch/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/wordsearch/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "3b63608ce499c2699288",
+    "url": "/wordsearch/static/js/runtime~main.3b63608c.js"
+  },
+  {
+    "revision": "bf4dd286824e189fb14f7bcec7e7e748",
+    "url": "/wordsearch/index.html"
+  }
+];
